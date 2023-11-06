24aa2562ac114a7a8d38f09663c95cfbfa44f5f0
Alex Whiteside <42042094+awhiteside1@users.noreply.github.com>
2023-11-06 05:23:40 -0500

    gger

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,55 @@
-export const x = 5
+'use strict'
+
+var dataForge = require('data-forge')
+
+var salesData = new dataForge.DataFrame([
+  {
+    ClientName: 'Ash',
+    Sales: 10,
+  },
+  {
+    ClientName: 'John',
+    Sales: 12,
+  },
+  {
+    ClientName: 'Ash',
+    Sales: 12,
+  },
+  {
+    ClientName: 'Ash',
+    Sales: 3,
+  },
+  {
+    ClientName: 'William',
+    Sales: 39,
+  },
+  {
+    ClientName: 'John',
+    Sales: 20,
+  },
+  {
+    ClientName: 'John',
+    Sales: 35,
+  },
+  {
+    ClientName: 'Ash',
+    Sales: 15,
+  },
+])
+
+// Group by client.
+var summarized = salesData
+    .groupBy((row) => row.ClientName)
+    .select((group) => ({
+      ClientName: group.first().ClientName,
+
+      // Sum sales per client.
+      Sales: group.deflate()
+    }))
+    // Series -> DataFrame.
+
+summarized.bake()
+
+console.log('-- Output as JavaScript array: ')
+var asJSArray = summarized.toArray()
+console.log(asJSArray)
